2881734d9a775c03df55cc7ad8c03adcd20a556c
Chip Bell <chip.bell@spinen.com>
2015-02-02 20:32:15 -0500

    Capturing CDF bug in a test

--- test.js
+++ test.js
@@ -2,3 +2,6 @@ var G = require('./index.js');
 var assert = require('assert');
 
 assert.ok(G.pdf(0, 0.9, 2) < Infinity);
+
+var result = G.cdf(0, 0.2, 2);
+assert.ok(result == result, 'CDF at 0 got NaN');
